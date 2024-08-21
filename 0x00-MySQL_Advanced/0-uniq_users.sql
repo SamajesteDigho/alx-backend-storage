424a1506b2f00bf9175848174aa952cfbc6100ad
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-22 06:11:41 +0800

    Pushing exercises 1, 2 and 3

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- For: Create USERS table on the database hbtn_0d_tvshows
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255)
 );
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- For: Select in a grouping manner
-SELECT origin, SUM(fans) AS nb_fans
+SELECT `origin`, SUM(`fans`) AS nb_fans
 FROM metal_bands
-GROUP BY origin
+GROUP BY `origin`
 ORDER BY nb_fans DESC;
