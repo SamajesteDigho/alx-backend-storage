006cf27bf0644751052730639f26cb0673c273b2
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-22 14:03:29 +0800

    Pushing Exo 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,1 +1,11 @@
--- For: 
+-- For: Create safeDiv function and return value
+DELIMITER $$
+CREATE
+FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END $$
