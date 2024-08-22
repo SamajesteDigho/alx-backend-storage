08c186d3cedf6c0e6af897dde071f112c1c772ff
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-22 08:44:33 +0800

    Pushing Exo 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,1 +1,11 @@
--- For: 
+-- For: Trigger for email validation
+DELIMITER $$
+CREATE
+TRIGGER `validate_email_AINS`
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
