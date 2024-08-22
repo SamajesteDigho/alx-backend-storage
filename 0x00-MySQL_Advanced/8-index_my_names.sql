c6d895b54956816b9c1b6905d4ceed4dcfc89d07
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-22 13:34:41 +0800

    Pushing Exo 9

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
--- For: 
+-- For: Indexing the name field so as to make it rapidly searcheable
 CREATE INDEX idx_name_first ON names (name(1));
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,1 +1,7 @@
--- For: 
+-- For: Creating indexing on name and score fields
+CREATE
+INDEX idx_name_first_score
+ON names (
+    names(1),
+    score(1)
+)
