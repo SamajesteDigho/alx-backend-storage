fa09393f6bacabbc5093b61d3a5cacf74a72e105
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-22 10:40:38 +0800

    Pushing exo 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,1 +1,14 @@
--- For: 
+-- For: Create procedure to add new row on table under certain circumstances
+DELIMITER $$
+CREATE PROCEDURE addBonus(
+    user_id INT,
+    project_name VARCHAR(255),
+    score FLOAT
+)
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM projects WHERE name = project_name) THEN
+        INSERT INTO projects(name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name LIMIT 1), score);
+END $$
