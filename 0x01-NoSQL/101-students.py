4c1ae9251c40fc9c5daa8ec17aa757c301fe94dd
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-28 07:51:27 +0800

    Exo101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Here the module for bonus question 101
+"""
+
+
+def top_students(mongo_collection):
+    """ Get the top students by thier averages """
+    scores = mongo_collection.aggregate([
+        {"$unwind": "$topics"},
+        {"$project": {"score": "$topics.score"}},
+        {"$group": {"_id": None, "average": {"$avg": "$score"}}}
+    ])
+    for x in scores:
+        mongo_collection.update_one(
+            {"_id": x['_id']},
+            {"$set": {"averageScore": x["average"]}}
+        )
+    return mongo_collection.find()
