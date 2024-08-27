d5e9886ae63780d42178aaf5b86e0ce8f71205cd
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-28 05:49:22 +0800

    Commiting NoSQL Exo 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,3 +2,9 @@
 """
     Here the module description of Exo 11
 """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ Schools offering a specific course """
+    res = mongo_collection.find({"topics": topic})
+    return res
