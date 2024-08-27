e9716189acfa89042c53c62eac3645ee31b7479e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-28 05:40:58 +0800

    Commiting NoSQL Exo 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,4 +1,13 @@
-#!/ussr/bin/env python3
+#!/usr/bin/env python3
 """
     Here the module description of Exo 10
 """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Change topics based on the name """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}},
+        upsert=False
+    )
