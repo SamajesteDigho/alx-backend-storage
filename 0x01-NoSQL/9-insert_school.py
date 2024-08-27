62de317983f004fab8441f9a213d6c0436820edd
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-28 05:27:27 +0800

    Commiting NoSQL Exo 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,4 +1,10 @@
-#!/ussr/bin/env python3
+#!/usr/bin/env python3
 """
     Here the module description of Exo 09
 """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Insert a new document in a collection """
+    new = mongo_collection.insert_one(kwargs)
+    return new.inserted_id
