6aab95e7092a94ff296e30d6e49102ab38dcc70d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-29 06:51:30 +0800

    Pushing exercise 0 and 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,31 +9,30 @@ import redis
 
 class Cache():
     """ The cache class for monitoring some stored data """
-    
+
     def __init__(self) -> None:
         """ Initialization process of this class instance """
         self._redis = redis.Redis()
-    
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store methode procedure and descriptions """
         key: str = str(uuid4())
         self._redis.set(name=key, value=data)
         return key
 
-
-    def get(self, key: str, fn: Union[Callable, None] = None) -> Union[str, bytes, int, float]:
+    def get(self,
+            key: str,
+            fn: Union[Callable, None] = None
+            ) -> Union[str, bytes, int, float]:
         """ Lets define the get method procedure """
         data = self._redis.get(key)
         if fn is not None:
             return fn(data)
         return data
 
-
     def get_int(self, key: str) -> int:
         """ Get integer value from redis """
         return self.get(key=key, fn=int)
-
 
     def get_str(self, key: str) -> str:
         """ Get integer value from redis """
